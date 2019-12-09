797e5c894aa5eb04d8deff15fa94d5bca6d32b99
Jim Balhoff <jim@balhoff.org>
2019-12-08 20:17:24 -0500

    Preliminary code for reading hypothetical ontology.

--- build.sbt
+++ build.sbt
@@ -22,6 +22,7 @@ libraryDependencies ++= {
   Seq(
     "org.geneontology"       %% "whelk"               % "0.4",
     "dev.zio"                %% "zio"                 % "1.0.0-RC17",
+    "org.phenoscape"         %% "scowl"               % "1.3.4",
     "net.sourceforge.owlapi" %  "owlapi-distribution" % "4.5.14",
     "commons-codec"          %  "commons-codec"       % "1.13"
   )
--- src/main/scala/org/monarchinitiative/boomer/OntUtil.scala
+++ src/main/scala/org/monarchinitiative/boomer/OntUtil.scala
@@ -5,17 +5,30 @@ import java.util.UUID
 
 import org.apache.commons.codec.digest.DigestUtils
 import org.geneontology.whelk.BuiltIn.Bottom
-import org.geneontology.whelk.{AtomicConcept, ConceptInclusion, Conjunction}
+import org.geneontology.whelk.{AtomicConcept, Axiom, Bridge, ConceptInclusion, Conjunction}
 import org.monarchinitiative.boomer.Boom.BoomError
 import org.monarchinitiative.boomer.Model.{AlternativesGroup, Proposal}
+import org.openrdf.model.vocabulary.DCTERMS
+import org.semanticweb.owlapi.apibinding.OWLManager
+import org.semanticweb.owlapi.model.{IRI, OWLAnnotation, OWLAnnotationProperty, OWLAnnotationSubject, OWLAnnotationValue, OWLAnonymousIndividual, OWLAxiom, OWLIndividual, OWLNamedIndividual, OWLOntology, OWLSubClassOfAxiom}
+import org.semanticweb.owlapi.model.parameters.Imports
 import zio._
 import zio.blocking._
 
 import scala.io.Source
+import scala.jdk.CollectionConverters._
+import org.phenoscape.scowl._
+import org.semanticweb.owlapi.search.EntitySearcher
+import org.semanticweb.owlapi.vocab.{DublinCoreVocabulary, OWLRDFVocabulary}
 
 object OntUtil {
 
   val DisjointSiblingPrefix = "http://boom.monarchinitiative.org/vocab/disjoint_sibling#"
+  val HasProbability = "http://semanticscience.org/resource/SIO_000638"
+  private val HasProbabilityAP = AnnotationProperty(HasProbability)
+  private val IsPartOfAP = AnnotationProperty(DCTERMS.IS_PART_OF.stringValue)
+  private val AlternativesGroupClass = Class("") //FIXME
+  private val ProposalClass = Class("") //FIXME
 
   def readPTable(file: File): ZIO[Blocking, Throwable, Set[AlternativesGroup]] = for {
     source <- Task.effect(Source.fromFile(file, "utf-8"))
@@ -47,6 +60,64 @@ object OntUtil {
         ).filter(_.probability > 0.0))
       }
     } else Task.fail(BoomError(s"Invalid ptable line: $line"))
+  }
+
+  final case class ProbabilisticOntology(axioms: Set[Axiom], hypotheticals: Set[AlternativesGroup])
+
+  object ProbabilisticOntology {
+
+    val empty = ProbabilisticOntology(Set.empty, Set.empty)
+
+  }
+
+  def readProbabilisticOntology(file: File): ZIO[Blocking, Throwable, ProbabilisticOntology] = for {
+    manager <- Task.effect(OWLManager.createOWLOntologyManager())
+    inFile <- Task.effect(IRI.create(file))
+    ontology <- effectBlocking(manager.loadOntology(inFile))
+    po <- partitionOntology(ontology)
+  } yield po
+
+  private def partitionOntology(ontology: OWLOntology): Task[ProbabilisticOntology] = {
+    val groups = EntitySearcher.getInstances(AlternativesGroupClass, ontology).asScala.toSet[OWLIndividual].map(_ -> Set.empty[Proposal]).toMap
+    val proposals: Task[(Map[OWLIndividual, Set[Proposal]], Set[OWLAxiom])] =
+      EntitySearcher.getInstances(ProposalClass, ontology).asScala.toSet.foldLeft(Task.effect(groups -> Set.empty[OWLAxiom])) {
+        case (acc, ind) =>
+          val proposalAnnotationSubject = ind match {
+            case anon: OWLAnonymousIndividual => anon
+            case named: OWLNamedIndividual    => named.getIRI
+          }
+          val maybeProbability = ZIO.fromOption(EntitySearcher.getAnnotations(proposalAnnotationSubject, ontology, HasProbabilityAP).asScala.toSet[OWLAnnotation].collectFirst {
+            case Annotation(_, HasProbabilityAP, value ^^ XSDDouble) =>
+              ZIO.fromOption(value.toDoubleOption)
+                .mapError(_ => BoomError(s"Proposal has probability value that can't be converted to a double: $proposalAnnotationSubject"))
+          }).mapError(_ => BoomError(s"Can't find probability for proposal: $proposalAnnotationSubject")).flatten
+          val proposalLabel = EntitySearcher.getAnnotations(proposalAnnotationSubject, ontology, RDFSLabel).asScala.toSet[OWLAnnotation].collectFirst {
+            case Annotation(_, RDFSLabel, label ^^ _) => label
+          }.getOrElse("")
+          val proposalOWLAxioms = ontology.getAxioms(Imports.INCLUDED).asScala.toSet[OWLAxiom].collect {
+            case axiom if axiom.getAnnotations(IsPartOfAP).asScala.exists(_.getValue == proposalAnnotationSubject) => axiom
+          }
+          val proposalWhelkAxioms = proposalOWLAxioms.flatMap(Bridge.convertAxiom).collect { case ci: ConceptInclusion => ci }
+          val maybeGroup = ZIO.fromOption(EntitySearcher.getAnnotations(proposalAnnotationSubject, ontology, IsPartOfAP).asScala.toSet[OWLAnnotation].collectFirst {
+            case Annotation(_, IsPartOfAP, anon: OWLAnonymousIndividual) => anon
+            case Annotation(_, IsPartOfAP, iri: IRI)                     => Individual(iri)
+          }).mapError(_ => BoomError(s"Can't find group for proposal: $proposalAnnotationSubject"))
+          val maybeProposal = maybeProbability.map(p => Proposal(proposalLabel, proposalWhelkAxioms, p))
+          for {
+            (groups, axiomsToRemove) <- acc
+            group <- maybeGroup
+            proposal <- maybeProposal
+          } yield {
+            val currentProposals = groups.getOrElse(group, Set.empty)
+            groups.updated(group, currentProposals + proposal) -> (axiomsToRemove ++ proposalOWLAxioms)
+          }
+      }
+    proposals.map {
+      case (proposalGroups, axiomsToRemove) =>
+        val ontAxioms = (ontology.getAxioms(Imports.INCLUDED).asScala.toSet -- axiomsToRemove).flatMap(Bridge.convertAxiom)
+        val groups = proposalGroups.values.map(ps => AlternativesGroup(ps)).toSet
+        ProbabilisticOntology(ontAxioms, groups)
+    }
   }
 
   def negateConceptInclusion(axiom: ConceptInclusion): Task[Set[ConceptInclusion]] = for {
